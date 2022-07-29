3d0260886140e6f9365a32a6cb12d46d58d89c07
Pietra Tenório <tenoriopietra@gmail.com>
2022-07-29 11:24:32 -0300

    adicionando projeto

---
+++ javascript/nossas-potencias.js
@@ -0,0 +1,19 @@
+const card = document.querySelectorAll(".card__inner");
+
+function flipCard() {
+  this.classList.toggle('is-flipped');
+}
+card.forEach((card) => card.addEventListener("click", flipCard));
+
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
+const navLogo = document.querySelector('#navbar__logo');
+
+// Display Mobile Menu
+const mobileMenu = () => {
+  menu.classList.toggle('is-active');
+  menuLinks.classList.toggle('active');
+};
+
+menu.addEventListener('click', mobileMenu);
+
